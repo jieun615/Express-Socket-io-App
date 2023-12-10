20e50c59b49f54c7b8b9336759358af551789ebe
jieun615 <je060105@gmail.com>
2023-12-11 08:36:22 +0900

    index.html 작성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
