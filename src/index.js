5f704a98674c87479ebc30b9cb2180f3d594d489
jieun615 <je060105@gmail.com>
2023-12-11 09:54:54 +0900

    socket.io 객체 생성

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 const port = process.env.SERVER_PORT;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Running on port ${port}`);
-});
+}); 
